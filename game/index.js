67f9de899ef5c58831a88c7fb54d55b47c3a78a8
Rick Terry <fdterr@gmail.com>
2018-07-05 17:00:39 -0400

    Working on error handling

--- game/index.js
+++ game/index.js
@@ -1,75 +1,134 @@
-import {Map} from 'immutable'
-let board = Map()
+import { Map } from 'immutable';
+let board = Map();
 
 const move = (player, position) => {
-  return { type: 'MOVE', player: player, position: position }
-}
+  return { type: 'MOVE', player: player, position: position };
+};
 
 const streak = (streakBoard, coords) => {
-  let result = streakBoard.getIn(coords[0])
+  let result = streakBoard.getIn(coords[0]);
   if (result === '_') {
-    return null
+    return null;
   }
   for (let i = 1; i < 3; i++) {
     if (streakBoard.getIn(coords[i]) !== result) {
       // console.log('coord', coords[i], 'getIn', streakBoard.getIn(coords[i]))
-      return undefined
+      return undefined;
     }
   }
-  return result
-}
+  return result;
+};
 
-const winner = (thisBoard) => {
+const winner = thisBoard => {
   if (streak(thisBoard, [[0, 0], [1, 1], [2, 2]])) {
-    return streak(thisBoard, [[0, 0], [1, 1], [2, 2]])
+    return streak(thisBoard, [[0, 0], [1, 1], [2, 2]]);
   }
   if (streak(thisBoard, [[0, 2], [1, 1], [2, 0]])) {
-    return streak(thisBoard, [[0, 0], [1, 1], [2, 2]])
+    return streak(thisBoard, [[0, 0], [1, 1], [2, 2]]);
   }
 
-  let streakArr = []
+  let streakArr = [];
   for (let i = 0; i < 3; i++) {
-    let rowEl = []
-    let colEl = []
+    let rowEl = [];
+    let colEl = [];
     for (let j = 0; j < 3; j++) {
-      rowEl.push([i, j])
-      colEl.push([j, i])
+      rowEl.push([i, j]);
+      colEl.push([j, i]);
     }
-    streakArr.push(rowEl)
-    streakArr.push(colEl)
+    streakArr.push(rowEl);
+    streakArr.push(colEl);
   }
 
+  let result = undefined;
   streakArr.forEach(streakEl => {
-    // console.log('got into forEach with', streakEl)
-    // console.log(streak(thisBoard, streakEl))
     if (streak(thisBoard, streakEl)) {
-      console.log('entered if block on streak', streakEl, streak(thisBoard, streakEl))
-      return streak(thisBoard, streakEl)
+      result = streak(thisBoard, streakEl);
+      // let toReturn = streak(thisBoard, streakEl);
+      return result;
     }
-  })
+  });
+
+  if (result !== undefined) {
+    return result;
+  }
 
   for (let r = 0; r != 3; ++r) {
     for (let c = 0; c != 3; ++c) {
       if (!thisBoard.hasIn([r, c])) {
-        return null
+        return null;
       }
     }
   }
-  return 'draw'
-}
+  return 'draw';
+};
 
-
-const reducer = (state, action) => {
+const turnReducer = (state, action) => {
+  let result = bad(state, action);
+  console.log(result);
   switch (action.type) {
     case 'START':
-      return { board, turn: 'X' }
+      return 'X';
     case 'MOVE':
-
-      board = board.setIn(action.position, action.player)
-      if (action.player === 'X') return { board, turn: 'O' }
-      else return { board, turn: 'X' }
-    default: return state
+      if (action.player === 'X') return 'O';
+      else return 'X';
+    default:
+      return state;
   }
-}
+};
 
-module.exports = { reducer, move, winner, streak }
+const boardReducer = (state, action) => {
+  switch (action.type) {
+    case 'MOVE':
+      console.log('calling board.setIn');
+      board = board.setIn(action.position, action.player);
+      return board;
+    default:
+      return board;
+  }
+};
+
+const reducer = (state, action) => {
+  if (bad(state, action) === null) {
+    board = boardReducer(state, action);
+    let winningSymbol = winner(board);
+    return {
+      board,
+      turn: turnReducer(state, action),
+      winner: winningSymbol,
+    };
+  } else {
+    console.log('hit error block');
+    console.log(bad(state, action));
+    return {
+      ...state,
+      error: bad(state, action),
+    };
+  }
+};
+
+const bad = (state, action) => {
+  switch (action.type) {
+    case 'START':
+      return null;
+    // if (action.player || action.position) {
+    //   return 'Error, start function contains extra arguments';
+    // }
+    case 'MOVE':
+      console.log('**** action', action.position);
+      if (action.position.length !== 2) {
+        return 'Error, position is not a 2 element array';
+      } else if (board.getIn(action.position)) {
+        console.log('current position: ', action.position);
+        console.log(
+          'getIn of current position: ',
+          board.getIn(action.position)
+        );
+        return 'Error, position is already filled';
+      }
+      return null;
+    default:
+      return null;
+  }
+};
+
+module.exports = { reducer, move, winner, streak };
--- index.js
+++ index.js
@@ -1,67 +1,73 @@
-import inquirer from 'inquirer'
-import {Map} from 'immutable'
+import inquirer from 'inquirer';
+import { Map } from 'immutable';
 
-import gameReducer from './game'
-import {createStore} from 'redux'
+import gameReducer from './game';
+import { createStore } from 'redux';
 
 const ongoing = Map()
-  .setIn([0, 0], 'X')
-  .setIn([1, 0], 'O')
-  .setIn([0, 1], 'X')
-  .setIn([1, 1], 'O')
+  .setIn([0, 0], 'O')
+  .setIn([1, 0], 'X')
+  .setIn([0, 1], 'O')
+  .setIn([1, 1], 'X');
 
-const xWins = ongoing
-  .setIn([0, 2], 'X')
+const xWins = ongoing.setIn([0, 2], 'O');
 
 const oWins = ongoing
-  .setIn([0, 0], 'O')
-  .setIn([1, 1], 'O')
-  .setIn([2, 2], 'O')
+  .setIn([0, 0], 'X')
+  .setIn([1, 1], 'X')
+  .setIn([2, 2], 'X');
 
-
-console.log('null?', gameReducer.winner(ongoing))
-console.log('X?', gameReducer.winner(xWins))
-console.log('O?', gameReducer.winner(oWins))
+console.log('null?', gameReducer.winner(ongoing));
+console.log('O?', gameReducer.winner(xWins));
+console.log('X?', gameReducer.winner(oWins));
 
 const printBoard = () => {
-  const {board} = game.getState()
+  const { board } = game.getState();
   for (let r = 0; r != 3; ++r) {
     for (let c = 0; c != 3; ++c) {
-      process.stdout.write(board.getIn([r, c], '_'))
+      process.stdout.write(board.getIn([r, c], '_'));
     }
-    process.stdout.write('\n')
+    process.stdout.write('\n');
   }
-}
+};
 
 const getInput = player => async () => {
-  const {turn} = game.getState()
-  if (turn !== player) return
-  const ans = await inquirer.prompt([{
-    type: 'input',
-    name: 'coord',
-    message: `${turn}'s move (row,col):`
-  }])
-  const [row=0, col=0] = ans.coord.split(/[,\s+]/).map(x => +x)
-  game.dispatch(gameReducer.move(turn, [row, col]))
-}
+  const { turn } = game.getState();
+  if (turn !== player) return;
+  const ans = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'coord',
+      message: `${turn}'s move (row,col):`,
+    },
+  ]);
+  const [row = 0, col = 0] = ans.coord.split(/[,\s+]/).map(x => +x);
+  game.dispatch(gameReducer.move(turn, [row, col]));
+};
 
 const printWinner = () => {
-  const {board} = game.getState()
-  return gameReducer.winner(board)
-}
+  const { board } = game.getState();
+  return gameReducer.winner(board);
+};
 
 // Create the store
-const game = createStore(gameReducer.reducer)
+const game = createStore(gameReducer.reducer);
 
 // Debug: Print the state
 // game.subscribe(() => console.log(game.getState()))
 
-game.subscribe(printBoard)
-game.subscribe(getInput('X'))
-game.subscribe(getInput('O'))
-
-game.subscribe(printWinner)
+game.subscribe(printBoard);
+game.subscribe(() => game.getState());
+game.subscribe(getInput('X'));
+game.subscribe(getInput('O'));
+game.subscribe(() => {
+  if (game.getState().winner !== null) {
+    console.log(game.getState().winner, 'is the winner!');
+    process.exit(0);
+  }
+});
+game.subscribe(printWinner);
 
 // We dispatch a dummy START action to call all our
 // subscribers the first time.
-game.dispatch({ type: 'START' })
+game.dispatch({ type: 'START' });
